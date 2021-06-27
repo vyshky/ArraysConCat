0360b564c95a25a66666fb0bbf1692efb64d995e
Alexander Dedykhin <vyshky2@gmail.com>
2021-06-27 17:44:55 +0400

    DONE

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            
+            A = Convert.ToInt32(Console.ReadLine());
+            B = Convert.ToInt32(Console.ReadLine());
+            if (A < B)
+            {
+                for (int i = A; i <= B; i++)
+                {
+                    for (int j = 0; j < i; j++) Console.Write(i);
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
