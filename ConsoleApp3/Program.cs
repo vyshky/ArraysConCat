ad15b68a5f378b77f1916516eb6ba057c3b0b701
vyshky <vyshky2@Gmail.com>
2021-06-27 18:42:59 +0400

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp3
 {
@@ -6,19 +6,45 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            int A;
-            int B;
-            
-            A = Convert.ToInt32(Console.ReadLine());
-            B = Convert.ToInt32(Console.ReadLine());
-            if (A < B)
-            {
-                for (int i = A; i <= B; i++)
-                {
-                    for (int j = 0; j < i; j++) Console.Write(i);
+            int n = 10, m = 5;
+            int[] array1 = new int[n];
+            int[] array2 = new int[m];
+            int[] array3 = new int[n + m];
+            _Random(array1);
+            _Random(array2);
 
-                    Console.WriteLine();
-                }
+            Print(array1);
+            Console.Write($" + ");
+            Print(array2);
+            Console.Write($" = ");
+
+            Array.Copy(array1, array3, n);
+            ConCat(array3, array2);
+            Print(array3);
+        }
+
+        static void _Random(int[] array)
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 10);
+            }
+        }
+
+        static void Print(int[] arr)
+        {
+            foreach (var i in arr)
+            {
+                Console.Write(i);
+            }
+        }
+
+        static void ConCat(int[] array1, int[] array2)
+        {
+            for (int i = array1.Length - array2.Length; i < array1.Length; i++)
+            {
+                array1[i] = array2[i - (array1.Length - array2.Length)];
             }
         }
     }
